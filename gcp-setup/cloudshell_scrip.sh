5869edc52cac300f8eea3af1d8af5f176ed3b644
Holden Karau <holden@pigscanfly.ca>
2019-01-12 23:22:50 -0800

    Remove extra back tick (caught by shellcheck)

--- gcp-setup/cloudshell_scrip.sh
+++ gcp-setup/cloudshell_scrip.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Note: this only works inside of cloudshell!
 #tag::cloudshell_script[]
-cloudshell_open --repo_url "https://source.developers.google.com/p/$PROJECTID/r/$PROJECTID-$DEPLOYMENTNAME-config" --dir"v$KUBEFLOWVERSION/kubeflow/kf_util" --page "editor" --tutorial "conn.md"`
+cloudshell_open --repo_url "https://source.developers.google.com/p/$PROJECTID/r/$PROJECTID-$DEPLOYMENTNAME-config" --dir"v$KUBEFLOWVERSION/kubeflow/kf_util" --page "editor" --tutorial "conn.md"
 #end::cloudshell_script[]
