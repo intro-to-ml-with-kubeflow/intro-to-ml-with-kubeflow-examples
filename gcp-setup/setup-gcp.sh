0289b1a6f914ee32ba17cc2a34dfc2187e52d92e
Holden Karau <holden@pigscanfly.ca>
2019-01-12 23:19:03 -0800

    Install gcp sdk

--- gcp-setup/setup-gcp.sh
+++ gcp-setup/setup-gcp.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+#tags::ubuntu[]
+apt-get install google-cloud-sdk
+#end::ubuntu[]
+apt-get remove google-cloud-sdk
+#tags::general[]
+curl https://sdk.cloud.google.com | bash
+#end::general[]
