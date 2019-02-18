6074a8e1bc0caec3bbc12fc7f6463124a58306bf
Holden Karau <holden@pigscanfly.ca>
2019-02-17 16:47:31 -0800

    Add a long line check.

--- ci.sh
+++ ci.sh
@@ -2,3 +2,11 @@
 
 # Check all the shell scripts
 find ./ -iregex '^.+\.??sh$' -type f -print0 | xargs -0 shellcheck
+
+# Check for long lines
+longlines=$(grep '.\{100\}' -r ./)
+
+if [[ ! -z "$longlines" ]]; then
+  echo "Error found lines longer than allowed. $longlines"
+  exit 1
+fi
