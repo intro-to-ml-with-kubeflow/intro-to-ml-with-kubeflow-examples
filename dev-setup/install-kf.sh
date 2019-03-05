76358ab50413a9c43f4f79b1ca5b5b3d1817c45a
Holden Karau <holden@pigscanfly.ca>
2019-03-04 19:52:24 -0800

    Update install to make the directory

--- dev-setup/install-kf.sh
+++ dev-setup/install-kf.sh
@@ -2,10 +2,13 @@
 #tag::install[]
 #Configuration
 export KUBEFLOW_DIR=~/kf_src
-export KUBEFLOW_TAG=v0.4.0
+export KUBEFLOW_TAG=v0.4.1
 # ^ You can also point this to master if you want to try
+# Create the directory for Kubeflow to live in
+mkdir -p $KUBEFLOW_DIR
+pushd $KUBEFLOW_DIR
 curl https://raw.githubusercontent.com/kubeflow/kubeflow/${KUBEFLOW_TAG}/scripts/download.sh | bash
-# Optionally add the scripts directory to your path to simplify
+# Optionally add the scripts directory to your path to simplify your work
 export PATH=$PATH:$KUBEFLOW_DIR/scripts/
+popd
 #end::install[]
-
