cbb6f33d3140c826f5647795264742aae4f5e08d
Holden Karau <holden@pigscanfly.ca>
2019-03-04 19:55:01 -0800

    Remove manual argos setup

--- ch2_seldon_examples/setup_example.sh
+++ ch2_seldon_examples/setup_example.sh
@@ -16,10 +16,6 @@ kubectl -n kube-system create sa tiller
 kubectl create clusterrolebinding tiller --clusterrole cluster-admin --serviceaccount=kube-system:tiller
 helm init --service-account tiller
 kubectl rollout status deploy/tiller-deploy -n kube-system
-#TODO(trevor):
-# Setup Argo (is this needed? it feels like it shouldn't be)
-kubectl create clusterrolebinding my-cluster-admin-binding --clusterrole=cluster-admin --user=$(gcloud info --format="value(config.account)")
-kubectl create clusterrolebinding default-admin2 --clusterrole=cluster-admin --serviceaccount=kubeflow:default
 # Setup SAD
 helm install seldon-core-analytics --name seldon-core-analytics --set grafana_prom_admin_password=password --set persistence.enabled=false --repo https://storage.googleapis.com/seldon-charts --namespace kubeflow
 #end::setup_components[]
