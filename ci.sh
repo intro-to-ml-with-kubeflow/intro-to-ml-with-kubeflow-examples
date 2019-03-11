680c30ea38e08d0b36fdf2d5f7bba4d71b1ecd67
Holden Karau <holden@pigscanfly.ca>
2019-03-10 17:05:37 -0700

    Update ci script to check for tags and also fix shellcheck invocation to skip missing sourced files because we need to source the ebnv script often

--- ci.sh
+++ ci.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+set -ex
+
 # Check all the shell scripts
-find ./ -iregex '^.+\.??sh$' -type f -print0 | xargs -0 shellcheck
+find ./ -iregex '^.+\.sh$' -type f -print0 | xargs -0 shellcheck -e SC1091
+# Check for cases where I use tags rather than tag
+bad_tags=$(grep -r "tags::" ./ | grep -v "ci.sh:")
+if [[ ! -z "$bad_tags" ]]; then
+  echo "Found bad tags $bad_tags replace tags with tag"
+  exit 1
+fi
