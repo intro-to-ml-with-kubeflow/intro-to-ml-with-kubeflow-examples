a02d9fa38a3ec63da03f1e6312c0f198893c3e0a
Holden Karau <holden@pigscanfly.ca>
2019-03-10 18:45:30 -0700

    Update the setup gcp to include launching and deleting cluster from the command line as well as the click-to-deploy

--- gcp-setup/setup-gcp.sh
+++ gcp-setup/setup-gcp.sh
@@ -6,3 +6,28 @@ apt-get remove google-cloud-sdk
 #tag::general[]
 curl https://sdk.cloud.google.com | bash
 #end::general[]
+#tag::enable_container_apis[]
+gcloud services enable container.googleapis.com
+#end::enable_container_apis[]
+PROJECT_ID="boos-demo-projects-are-rad"
+#tag::configure_cloud_sdk[]
+gcloud auth login # Launches a web browser to login with
+gcloud config set project "$PROJECT_ID" #Project ID is your Google project ID
+#end::configure_cloud_sdk[]
+ZONE="us-west1-b"
+CLUSTER_NAME="ci-cluster"
+#tag::launch_cluster[]
+gcloud beta container clusters create $CLUSTER_NAME \
+       --zone $ZONE \
+       --machine-type "n1-standard-4" \
+       --disk-type "pd-standard" \
+       --disk-size "100" \
+       --scopes "https://www.googleapis.com/auth/cloud-platform" \
+       --addons HorizontalPodAutoscaling,HttpLoadBalancing \
+       --enable-autoupgrade \
+       --enable-autorepair \
+       --enable-autoscaling --min-nodes 1 --max-nodes 10 --num-nodes 2
+#end::launch_cluster[]
+#tag::delete_cluster[]
+gcloud beta container clusters delete $CLUSTER_NAME --zone $ZONE
+#end::delete_cluster[]
