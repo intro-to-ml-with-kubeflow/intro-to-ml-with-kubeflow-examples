aed259aa54a3720dbe269ccc45cdd08018116c64
Holden Karau <holden@pigscanfly.ca>
2019-03-13 22:42:16 -0700

    Set up a GKE cluster and azure

--- multi-cloud/README.md
+++ multi-cloud/README.md
@@ -10,7 +10,7 @@ If you don't already have a username and password for Google Cloud & Azure from 
 
 Kubeflow can be installed and deployed on many enivorments.
 For today's tutorial we will focus on using Google & Azure.
-The provided set up script is designed to be used within a Google Cloud Console instance, however you are free to modify it to run locally.
+The provided set up script is designed to be used within a Google Cloud Console instance, however you are free to modify it to run locally or do your own set-up.
 
 
 While there are many ways to set up Kubeflow, in the interest of spead we will start with using a fast setup script in this directory (`fast_setup.sh`).
@@ -22,6 +22,8 @@ While there are many ways to set up Kubeflow, in the interest of spead we will s
 * Set up a GKE and EKS cluster (named google-kf-test & azure-kf-test)
 
 At that point it's going to be on you to start your kubeflow adventure!
+
+Note: Kubeflow's control script also has the ability to create "platform" configuration, our fast set up script does not use this since it requires setting up an Identity Aware Proxy, which can add an extra 20 minutes (normally well worth, but today we're in a rush).
 
 ## Starting a Kubeflow project
 
--- multi-cloud/fast-start.sh
+++ multi-cloud/fast-start.sh
@@ -8,12 +8,43 @@ export KUBEFLOW_TAG=v0.4.1
 mkdir -p $KUBEFLOW_SRC
 pushd $KUBEFLOW_SRC
 curl https://raw.githubusercontent.com/kubeflow/kubeflow/${KUBEFLOW_TAG}/scripts/download.sh | bash
-export KF_SCRIPTS=`pwd`/scrtips
-
+echo "Adding to the path"
+export KF_SCRIPTS=`pwd`/scripts
+export PATH=$PATH:$KF_SCRIPTS
 echo "export PATH=$PATH:$KF_SCRIPTS" >> ~/.bashrc
 
-echo "Setting up GKE & file APIs"
-gcloud 
+echo "Enabling Google Cloud APIs"
+gcloud services enable file.googleapis.com storage-component.googleapis.com \
+       storage-api.googleapis.com stackdriver.googleapis.com containerregistry.googleapis.com \
+       iap.googleapis.com compute.googleapis.com container.googleapis.com
 echo "Setting up Azure"
-curl -L https://aka.ms/InstallAzureCli | bash
+sudo apt-get install apt-transport-https lsb-release software-properties-common dirmngr -y
+AZ_REPO=$(lsb_release -cs)
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | \
+  sudo tee /etc/apt/sources.list.d/azure-cli.list
+sudo apt-key --keyring /etc/apt/trusted.gpg.d/Microsoft.gpg adv \
+     --keyserver packages.microsoft.com \
+     --recv-keys BC528686B50D79E339D3721CEB3E94ADBE1229CF
+sudo apt-get update
+sudo apt-get install azure-cli
 az login
+
+echo "Starting up GKE cluster"
+GZONE="us-central1-a" # For TPU access
+GOOGLE_CLUSTER_NAME="google-kf-test"
+gcloud beta container clusters create $GOOGLE_CLUSTER_NAME \
+       --zone $GZONE \
+       --machine-type "n1-standard-8" \
+       --disk-type "pd-standard" \
+       --disk-size "100" \
+       --scopes "https://www.googleapis.com/auth/cloud-platform" \
+       --addons HorizontalPodAutoscaling,HttpLoadBalancing \
+       --enable-autoupgrade \
+       --enable-autorepair \
+       --enable-autoscaling --min-nodes 1 --max-nodes 10 --num-nodes 2 &
+GCLUSTER_CREATION_PID=$!
+echo "Starting up Azure K8s cluster"
+
+echo "Connecting to google cluster"
+wait $GCLUSTER_CREATION_PID || echo "cluster ready"
+gcloud beta container clusters delete $GCLUSTER_NAME --zone $GZONE
