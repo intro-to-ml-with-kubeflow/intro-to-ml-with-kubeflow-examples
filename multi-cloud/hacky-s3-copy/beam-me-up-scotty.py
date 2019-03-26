fbf8b3dbbb8c023a2a7652b34bcb161350a7b986
Trevor <trevor.d.grant@gmail.com>
2019-03-25 18:36:18 -0700

    doing work

--- multi-cloud/hacky-s3-copy/beam-me-up-scotty.py
+++ multi-cloud/hacky-s3-copy/beam-me-up-scotty.py
@@ -5,7 +5,7 @@ from ibm_botocore.client import Config
 BUCKET_NAME='demo-bucket123'
 # Constants for IBM COS values
 COS_ENDPOINT = 'https://s3.us-east.cloud-object-storage.appdomain.cloud'  ## This has to match s COS_BUCKET_LOCATION
-COS_API_KEY_ID = "DISJY3dwqhCYi8ZiKxQMdo28lbRBqsYG9F0ASAR0IgjB"
+COS_API_KEY_ID = "DISJY3dwqhCYi8ZiKxQMdo28  lbRBqsYG9F0ASAR0IgjB"
 COS_AUTH_ENDPOINT = "https://iam.cloud.ibm.com/identity/token"
 COS_RESOURCE_CRN = "crn:v1:bluemix:public:cloud-object-storage:global:a/1e27724d6a7feea015054bfe32fb52f5:39a172fc-d755-48b8-a24c-604c7c353df4::"
 COS_BUCKET_LOCATION = "us-east"  ## This has to match w COS_ENDPOINT
