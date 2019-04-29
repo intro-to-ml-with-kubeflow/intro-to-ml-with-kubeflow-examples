16fb1187b51d9921971fa1a1b6c52ad821511970
Holden Karau <holden@pigscanfly.ca>
2019-04-29 21:28:12 +0100

    Be clear solution7 has stop point swhich aren't automated

--- multi-cloud/solution7.sh
+++ multi-cloud/solution7.sh
@@ -10,6 +10,12 @@ wget https://raw.githubusercontent.com/intro-to-ml-with-kubeflow/intro-to-ml-wit
 wget https://raw.githubusercontent.com/intro-to-ml-with-kubeflow/intro-to-ml-with-kubeflow-examples/master/multi-cloud/hacky-s3-copy/beam-me-up-scotty.py
 wget https://raw.githubusercontent.com/intro-to-ml-with-kubeflow/intro-to-ml-with-kubeflow-examples/master/multi-cloud/hacky-s3-copy/requirements.txt
 
+echo "You are going to need to edit beam-up-scotty.py to point to your creds."
+echo "Press enter when done!"
+# shellcheck disable=2034
+read -r panda
+
+
 gcloud auth configure-docker
 GOOGLE_PROJECT=$(gcloud config get-value project 2>/dev/null)
 ##
@@ -18,6 +24,11 @@ docker build -t "gcr.io/$GOOGLE_PROJECT/hacky-s3-copy:oh-lord" .
 docker push "gcr.io/$GOOGLE_PROJECT/hacky-s3-copy:oh-lord"
 
 wget https://raw.githubusercontent.com/intro-to-ml-with-kubeflow/intro-to-ml-with-kubeflow-examples/master/multi-cloud/hacky-s3-copy/hacky-s3-copy.yaml
+
+echo "Yay now, your going to need to edit hacky-s3-copy.yaml to point to your GOOGLE_PROJECT"
+echo "Press enter when done"
+# shellcheck disable=2034
+read -r panda
 
 
 kubectl create -f hacky-s3-copy.yaml -n kubeflow
