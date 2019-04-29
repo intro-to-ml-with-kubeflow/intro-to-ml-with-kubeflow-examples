59186cc29e775d4f4e8f9a4450fa0b12f5019c51
Holden Karau <holden@pigscanfly.ca>
2019-04-29 16:09:16 +0100

    Clarify the build push reqs

--- multi-cloud/solution4.sh
+++ multi-cloud/solution4.sh
@@ -5,7 +5,9 @@ git clone https://github.com/kubeflow/example-seldon
 
 kubectl create -f https://raw.githubusercontent.com/rawkintrevo/intro-to-ml-with-kubeflow-examples/master/multi-cloud/config/pv-claim.yaml
 
-## The `-p build-push-image=true` works weather you _need_ to build the image or nah.
+## The `-p build-push-image=true` doesn't work unless you have your own docker
+## credentials stored in a K8s secret. Instead if you made a custom docker image you
+## can look at solution4b.sh
 
 cd example-seldon/workflows
 argo submit training-sk-mnist-workflow.yaml -n kubeflow -p build-push-image=false
