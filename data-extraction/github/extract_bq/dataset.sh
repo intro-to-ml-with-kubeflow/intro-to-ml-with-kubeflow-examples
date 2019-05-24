f4b21c990ded95834015c1eb35aa7366a2e5976c
Holden Karau <holden@pigscanfly.ca>
2019-05-24 05:43:51 -0700

    Simplify the dataset shell script

--- data-extraction/github/extract_bq/dataset.sh
+++ data-extraction/github/extract_bq/dataset.sh
@@ -4,8 +4,8 @@ set -ex
 
 
 export PROJECT=${PROJECT:=boos-demo-projects-are-rad}
-export DATASET=${DATASET:=intro_to_ml_with_kf_7}
-export BUCKET=${BUCKET:=kf-gh-demo-2}
+export DATASET=${DATASET:=intro_to_ml_with_kf}
+export BUCKET=${BUCKET:=kf-gh-demo}
 
 
 # Set up buckets and datasets
@@ -16,41 +16,18 @@ bq --location=us \
    --default_partition_expiration 3600000 \
    --description stackoverflow_data ${PROJECT}:$DATASET || true
 
-# These parts belong in a KF pipeline, we'll cover them next
-bq query --location=us \
-   --destination_table ${PROJECT}:${DATASET}.stackoverflow \
-   --replace \
-   --use_legacy_sql=false \
-   "$(cat stack_overflow_questions.bsql)"
-
-bq query --location=us \
-   --destination_table ${PROJECT}:${DATASET}.github_comments \
-   --replace \
-   --use_legacy_sql=false \
-   "$(cat github_comments_query_r2.bsql)"
-
-bq query --location=us \
-   --destination_table ${PROJECT}:${DATASET}.github_issues \
-   --replace \
-   --use_legacy_sql=false \
-   "$(cat github_issues_query.bsql)"
-
-bq query --location=us \
-   --destination_table ${PROJECT}:${DATASET}.github_push_events \
-   --replace \
-   --use_legacy_sql=false \
-   `cat github_push_events.bsql`
-
-bq query --location=us \
-   --destination_table ${PROJECT}:${DATASET}.github_push_events \
-   --replace \
-   --use_legacy_sql=false \
-   `cat github_push_events.bsql`
-
-# And extract to avro
-for TABLE in "github_issues" "github_comments" "github_push_events" "stackoverflow"
-do
-  echo "Extracting $TABLE"
-  bq --location=us extract --destination_format=AVRO\
-     "${PROJECT}:${DATASET}.${TABLE}" "gs://${BUCKET}/data/${TABLE}/data-*.avro"
+# Run all of the queries in the container.
+# This way we can add more queries without changing our shell script.
+for QUERY_NAME in $(ls *.bsql) do
+	     # Run the query
+	     bq query --location=us \
+		--destination_table ${PROJECT}:${DATASET}.${QUERY_NAME} \
+		--replace \
+		--use_legacy_sql=false \
+		"$(cat ${QUERY_NAME})"
+	     # And extract the result to avro
+	     echo "Extracting $TABLE"
+	     bq --location=us extract --destination_format=AVRO\
+		"${PROJECT}:${DATASET}.${QUERY_NAME}" \
+		"gs://${BUCKET}/data/${QUERY_NAME}/data-*.avro"
 done
