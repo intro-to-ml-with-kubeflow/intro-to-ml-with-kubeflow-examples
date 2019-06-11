6927635ae183d72e6f20f503164ab956daa76e38
Holden Karau <holden@pigscanfly.ca>
2019-06-11 05:31:02 -0700

    Fix remaining shellcheck issues in multi-cloud

--- multi-cloud/helper.sh
+++ multi-cloud/helper.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 cecho(){
-    RED="\033[0;31m"
+    RED='\033[0;31m'
     GREEN='\033[0;32m'
     YELLOW='\033[1;33m'
     # ... ADD MORE COLORS
     NC='\033[0m' # No Color
 
-    printf "${!1}${2} ${NC}\n"
+    printf "%s%s %s\\n" "${!1}" "${2}" "${NC}"
 }
