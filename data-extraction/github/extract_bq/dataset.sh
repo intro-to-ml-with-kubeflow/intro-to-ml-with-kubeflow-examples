df3e7980020ff9867de8c8b93d3c6da0df6c1af9
Holden Karau <holden@pigscanfly.ca>
2019-06-11 05:24:51 -0700

    Fix up dataset.sh shell mistakes

--- data-extraction/github/extract_bq/dataset.sh
+++ data-extraction/github/extract_bq/dataset.sh
@@ -10,8 +10,8 @@ export EXPIRATION=${EXPIRATION:=259200}
 export RUN_ID=${RUN_ID:=1}
 
 # If we have a SA file activate it
-if [[ ! -z ${GOOGLE_APPLICATION_CREDENTIALS} &&  -f ${GOOGLE_APPLICATION_CREDENTIALS} ]]; then
-  gcloud auth activate-service-account --key-file=${GOOGLE_APPLICATION_CREDENTIALS}
+if [[ ! -z "${GOOGLE_APPLICATION_CREDENTIALS}" &&  -f "${GOOGLE_APPLICATION_CREDENTIALS}" ]]; then
+  gcloud auth activate-service-account --key-file="${GOOGLE_APPLICATION_CREDENTIALS}"
 else
   echo "No SA found (checked ${GOOGLE_APPLICATION_CREDENTIALS})"
 fi
@@ -34,34 +34,34 @@ bq --location=us \
 
 # Run all of the queries in the container.
 # This way we can add more queries without changing our shell script.
-for QUERY_NAME in $(ls *.bsql)
+for QUERY_NAME in ./*.bsql
 do
   # Strip out the trailing .bsql from the table name to comply with BQ restrictions
-  TABLE_NAME="${RUN_ID}$(echo ${QUERY_NAME}| cut -d "." -f 1)"
+  TABLE_NAME="${RUN_ID}$(echo "${QUERY_NAME}" | cut -d "." -f 1)"
   # Run the query
   bq query --location=us \
-     --destination_table ${PROJECT}:${DATASET}.${TABLE_NAME} \
+     --destination_table "${PROJECT}:${DATASET}.${TABLE_NAME}" \
      --replace \
      --use_legacy_sql=false \
-     "$(cat ${QUERY_NAME})" &
+     "$(cat "${QUERY_NAME}")" &
   query_pid=$!
-  echo "${query_pid}" > ${QUERY_NAME}.pid
+  echo "${query_pid}" > "${QUERY_NAME}.pid"
 done
-for QUERY_NAME in $(ls *.bsql)
+for QUERY_NAME in ./*.bsql
 do
-  wait $(cat ${QUERY_NAME}.pid)
-  rm ${QUERY_NAME}.pid
+  wait "$(cat "${QUERY_NAME}.pid")"
+  rm "${QUERY_NAME}.pid"
   # And extract the result to avro
   echo "Extracting $TABLE"
   # We use the * here so that we have multiple files, required for > 1GB
   bq --location=us extract --destination_format=AVRO\
      "${PROJECT}:${DATASET}.${TABLE_NAME}" \
      "gs://${BUCKET}/data/${TABLE_NAME}/data-*.avro" &
-  echo "${query_pid}" > ${QUERY_NAME}.export_pid
+  echo "${query_pid}" > "${QUERY_NAME}.export_pid"
 done
 # Wait for all the table exports to finish. If we wait on all it eats the errors
-for QUERY_NAME in $(ls *.bsql)
+for QUERY_NAME in ./*.bsql
 do
-  wait $(cat ${QUERY_NAME}.export_pid)
-  rm ${QUERY_NAME}.export_pid
+  wait "$(cat "${QUERY_NAME}.export_pid")"
+  rm "${QUERY_NAME}.export_pid"
 done
