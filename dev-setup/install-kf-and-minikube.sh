e799adeeb829d8b55d07f4073c534a6510b79638
Holden Karau <holden@pigscanfly.ca>
2019-08-31 14:30:41 -0700

    Remove deprecated installation techniques

--- dev-setup/install-kf-and-minikube.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#tag::work[]
-export KUBEFLOW_TAG=v0.4.1
-curl -O https://raw.githubusercontent.com/kubeflow/kubeflow/${KUBEFLOW_TAG}/scripts/setup-minikube.sh
-chmod +x setup-minikube.sh
-./setup-minikube.sh
-#end::work[]
