d864550338b9f52112475de8b9cfd0ba8326bd9b
Holden Karau <holden@pigscanfly.ca>
2019-08-31 14:16:45 -0700

    Start of IOT stuff

---
+++ data-extraction/iot/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+CONTAINER_REGISTRY="gcr.io/${PROJECT_NAME}"
+#tag::buildandpush[]
+TARGET="${CONTAINER_REGISTRY}/kf-steps/iot-extract:v2"
+docker build . -t ${TARGET}
+docker push "${TARGET}"
+#end::buildandpush[]
+#tag::run[]
+kubectl apply -f iot_extract_job.yaml
+#end::run[]
+#tag::verify[]
+kubectl get jobs |grep gh-data
+#end::verify[]
