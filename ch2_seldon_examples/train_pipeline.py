6af761276d475cc13637e2b8fda696467aee57d9
Holden Karau <holden@pigscanfly.ca>
2019-09-12 11:47:01 -0700

    Do serving in a container op, TODO: rewrtie to use the ResourceOp as in https://github.com/kubeflow/pipelines/blob/master/samples/contrib/seldon/mnist_tf.py

--- ch2_seldon_examples/train_pipeline.py
+++ ch2_seldon_examples/train_pipeline.py
@@ -8,10 +8,9 @@ import kfp.onprem as onprem
     description='A simple end to end sci-kit seldon kf pipeline'
 )
 def mnist_train_pipeline(
-        output,
-        project,
         docker_org="index.docker.io/seldonio",
-        container_version="0.2"):
+        train_container_version="0.2",
+        serve_container_version="0.1"):
 
     vop = dsl.VolumeOp(
         name="create_pvc",
@@ -21,9 +20,21 @@ def mnist_train_pipeline(
     volume = vop.volume
     train = dsl.ContainerOp(
         name='sk-train',
-        image=f"{docker_org}/skmnistclassifier_trainer:{container_version}",
+        image=f"{docker_org}/skmnistclassifier_trainer:{train_container_version}",
         pvolumes={"/data": volume})
+    serving = dsl.ContainerOp(
+        name="mnist-classifier-serving",
+        image=f"{docker_org}/skmnistclassifier_runtime:{serve_container_version}",
+        pvolumes={"/data": train.pvolume}).after(train)
 
+# If we're called directly create an expirement and run
 if __name__ == '__main__':
-  import kfp.compiler as compiler
-    
+    pipeline_func = mnist_train_pipeline
+    pipeline_filename = pipeline_func.__name__ + '.pipeline.zip'
+    import kfp.compiler as compiler
+    compiler.Compiler().compile(pipeline_func, pipeline_filename)
+    expirement_name = "cheese"
+    experiment = client.create_experiment(expirement_name)
+    run_name = pipeline_func.__name__ + ' run'
+    run_result = client.run_pipeline(experiment.id, run_name, pipeline_filename, arguments)
+    print(run_result)
