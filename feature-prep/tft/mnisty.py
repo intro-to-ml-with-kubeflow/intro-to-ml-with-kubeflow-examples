0a0beef53982a4da880eb98b87cbda2cd5d0beb4
Holden Karau <holden@pigscanfly.ca>
2019-09-21 15:15:49 -0700

    start writing down basic mnist pre process idea

--- feature-prep/tft/mnisty.py
+++ feature-prep/tft/mnisty.py
@@ -4,4 +4,6 @@ import tensorflow_transform as tft
 import tensorflow_transform.beam as tft_beam
 
 pixel_fields = ...
+label_field = ...
 def preprocessing_fn(inputs):
+    # TODO: pre-process the fields to 0 to 1 range inputs
