67210c00013c2e06dd3e8b6c68fbc89170f1b5d9
Holden Karau <holden@pigscanfly.ca>
2019-11-08 18:14:13 -0800

    More install fixes, kustomizes install somewhat differently these days

--- dev-setup/install-kustomize.sh
+++ dev-setup/install-kustomize.sh
@@ -4,13 +4,16 @@ if [ ! -f ~/bin/kustomize ]; then
   PLATFORM=$(uname) # Either Linux or Darwin
   export PLATFORM
   mkdir -p ~/bin
-  KUSTOMIZE_TAG=${KUSTOMIZE_TAG:-kustomize%2Fv3.3.0}
-  curl -s https://api.github.com/repos/kubernetes-sigs/kustomize/releases/tags/${KUTOMIZE_TAG} |\
+  KVERSION=${KVERSION:-3.3.0}
+  # The same project releases multiple kinds of artifacts
+  KUSTOMIZE_TAG=${KUSTOMIZE_TAG:-kustomize%2Fv$KVERSION}
+  curl -s https://api.github.com/repos/kubernetes-sigs/kustomize/releases/tags/${KUSTOMIZE_TAG} |\
     grep browser_download |\
     grep -i "${PLATFORM}" |\
     cut -d '"' -f 4 |\
     xargs curl -O -L
-  mv kustomize_*_amd64 ~/bin/kustomize
+  tar -xvf kustomize_*${KVERSION}*.tar.gz
+  mv kustomize ~/bin/kustomize
   chmod u+x ~/bin/kustomize
   # Add this + platform/version exports to your bashrc or move the ks bin into /usr/bin
   export PATH=$PATH:"~/bin"
