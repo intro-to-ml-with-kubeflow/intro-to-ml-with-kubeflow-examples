850a1483f28ae36820701c083ca8dd5533afb914
Holden Karau <holden@pigscanfly.ca>
2020-01-10 13:43:42 -0800

    Multi cloud example is out of date.

--- multi-cloud/hacky-s3-copy/Dockerfile
+++
@@ -1,6 +0,0 @@
-FROM python:3.7-alpine
-
-COPY beam-me-up-scotty.py beam-me-up-scotty.py
-COPY requirements.txt requirements.txt
-RUN pip install -r requirements.txt
-CMD python beam-me-up-scotty.py
--- multi-cloud/solution3.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-## Install Argo
-curl -sSL -o ~/argo https://github.com/argoproj/argo/releases/download/v2.2.1/argo-linux-amd64
-chmod +x ~/argo
-kubectl create clusterrolebinding sa-admin --clusterrole=cluster-admin --serviceaccount=kubeflow:default
-cd ~/
-PATH=$(pwd):$PATH
-export PATH
