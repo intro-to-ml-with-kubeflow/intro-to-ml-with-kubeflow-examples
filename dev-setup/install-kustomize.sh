de0b07b9d006eeeaab1ff5b3816123e9687a03c1
Holden Karau <holden@pigscanfly.ca>
2020-02-08 22:24:09 -0800

    Update to 0.7 dev setup

--- dev-setup/install-kustomize.sh
+++ dev-setup/install-kustomize.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
-if [ ! -f ~/bin/kustomize ]; then
-  #tag::kustomize[]
-  PLATFORM=$(uname) # Either Linux or Darwin
-  export PLATFORM
-  mkdir -p ~/bin
-  KVERSION=${KVERSION:-3.3.0}
-  # The same project releases multiple kinds of artifacts
-  KUSTOMIZE_TAG=${KUSTOMIZE_TAG:-kustomize%2Fv$KVERSION}
-  curl -s https://api.github.com/repos/kubernetes-sigs/kustomize/releases/tags/${KUSTOMIZE_TAG} |\
-    grep browser_download |\
-    grep -i "${PLATFORM}" |\
-    cut -d '"' -f 4 |\
-    xargs curl -O -L
-  tar -xvf kustomize_*${KVERSION}*.tar.gz
-  mv kustomize ~/bin/kustomize
-  chmod u+x ~/bin/kustomize
-  # Add this + platform/version exports to your bashrc or move the ks bin into /usr/bin
-  export PATH=$PATH:"~/bin"
-  #end::kustomize[]
-fi
+#tag::kustomize[]
+export KUSTOMIZE_VERSION=3.1.0
+PLATFORM=$(uname) # Either Linux or Darwin
+export PLATFORM
+mkdir -p ~/bin
+curl -s https://api.github.com/repos/kubernetes-sigs/kustomize/releases/latest |\
+  grep browser_download |\
+  grep -i "${PLATFORM}" |\
+  cut -d '"' -f 4 |\
+  xargs curl -O -L
+mv kustomize_*_amd64 ~/bin/kustomize
+chmod u+x ~/bin/kustomize
+# Add this + platform/version exports to your bashrc or move the ks bin into /usr/bin
+export PATH=$PATH:"~/bin"
+#end::kustomize[]
