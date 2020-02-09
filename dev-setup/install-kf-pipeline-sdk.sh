30748403142955107d54b8aa6a0bce8f17c917ec
Holden Karau <holden@pigscanfly.ca>
2020-02-08 22:24:09 -0800

    Update to 0.7 dev setup

--- dev-setup/install-kf-pipeline-sdk.sh
+++ dev-setup/install-kf-pipeline-sdk.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 # Put as inside a venv
-if [ ! -f /tmp/kfvenv ]; then
-  pushd /tmp
-  #tag::venv[]
-  virtualenv kfvenv --python python3
-  source kfvenv/bin/activate
-  #end::venv[]
-  popd
-  #tag::install[]
-  URL=https://storage.googleapis.com/ml-pipeline/release/latest/kfp.tar.gz
-  pip install "${URL}" --upgrade
-  #end::install[]
-else
-  source /tmp/kfvenv/bin/activate
-fi
+pushd /tmp
+#tag::venv[]
+virtualenv kfvenv --python python3
+source kfvenv/bin/activate
+#end::venv[]
+popd
+#tag::install[]
+URL=https://storage.googleapis.com/ml-pipeline/release/latest/kfp.tar.gz
+pip install "${URL}" --upgrade
+#end::install[]
--- dev-setup/install-kf.sh
+++ dev-setup/install-kf.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
-if [ ! -f ~/bin/kfctl ]; then
-  #tag::install[]
-  PLATFORM=$(uname) # Either Linux or Darwin
-  export PLATFORM
-  mkdir -p ~/bin
-  #Configuration
-  export KUBEFLOW_TAG=v0.7.0
-  # ^ You can also point this to a different version if you want to try
-  KUBEFLOW_BASE="https://github.com/kubeflow/kubeflow/releases/download"
-  KUBEFLOW_FILE="kfctl_${KUBEFLOW_TAG}_${PLATFORM}.tar.gz"
-  wget "${KUBEFLOW_BASE}/${KUBEFLOW_TAG}/${KUBEFLOW_FILE}"
-  tar -xvf "${KUBEFLOW_FILE}"
-  mv ./kfctl ~/bin/
-  rm "${KUBEFLOW_FILE}"
-  # Optionally add the scripts directory to your path to simplify your work
-  export PATH=$PATH:~/bin
-  #end::install[]
-fi
+#tag::install[]
+PLATFORM=$(uname) # Either Linux or Darwin
+export PLATFORM
+mkdir -p ~/bin
+#Configuration
+export KUBEFLOW_TAG=v0.7.0
+# ^ You can also point this to a different version if you want to try
+KUBEFLOW_BASE="https://github.com/kubeflow/kubeflow/releases/download"
+KUBEFLOW_FILE="kfctl_${KUBEFLOW_TAG}_${PLATFORM}.tar.gz"
+wget "${KUBEFLOW_BASE}/${KUBEFLOW_TAG}/${KUBEFLOW_FILE}"
+tar -xvf "${KUBEFLOW_FILE}"
+mv ./kfctl ~/bin/
+rm "${KUBEFLOW_FILE}"
+# Optionally add the scripts directory to your path to simplify your work
+export PATH=$PATH:~/bin
+#end::install[]
