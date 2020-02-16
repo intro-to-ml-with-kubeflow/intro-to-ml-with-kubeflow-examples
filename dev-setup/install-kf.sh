55f73c3152313327b2b8baf6006deeae789dba95
Holden Karau <holden@pigscanfly.ca>
2020-02-16 14:26:14 -0800

    Start updating the chapter 2 example to KF 1.0 RC4

--- dev-setup/install-kf.sh
+++ dev-setup/install-kf.sh
@@ -4,9 +4,10 @@ PLATFORM=$(uname) # Either Linux or Darwin
 export PLATFORM
 mkdir -p ~/bin
 #Configuration
-export KUBEFLOW_TAG=v0.7.0
+export KUBEFLOW_TAG=v1.0.0-rc4
 # ^ You can also point this to a different version if you want to try
-KUBEFLOW_BASE="https://github.com/kubeflow/kubeflow/releases/download"
+KUBEFLOW_BASE="https://github.com/kubeflow/kfctl/releases/download"
+# TODO(holden): Verify if the hash in the file name is now part of the name or if that's just in the RC
 KUBEFLOW_FILE="kfctl_${KUBEFLOW_TAG}_${PLATFORM}.tar.gz"
 wget "${KUBEFLOW_BASE}/${KUBEFLOW_TAG}/${KUBEFLOW_FILE}"
 tar -xvf "${KUBEFLOW_FILE}"
