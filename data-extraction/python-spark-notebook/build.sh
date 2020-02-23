f3303b23d507758f698267cd973bea6e3d3663e9
Holden Karau <holden@pigscanfly.ca>
2020-02-22 17:25:11 -0800

    Update the build

--- data-extraction/python-spark-notebook/build.sh
+++ data-extraction/python-spark-notebook/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Build a notebook with Spark
 set -ex
-V=${V:-"5"}
+V=${V:-"10"}
 REPO=${REPO:-"gcr.io/$PROJECT"}
 TARGET=${TARGET:-"$REPO/kubeflow/spark-notebook:v$V"}
 BASE=${BASE:-"gcr.io/kubeflow-images-public/tensorflow-1.15.2-notebook-cpu:1.0.0"}
@@ -22,8 +22,12 @@ popd
 # Add GCS to Spark images
 docker build --build-arg base=$SPARK_TARGET/spark:v2.4.5 -t "${SPARK_TARGET}/spark-with-gcs:v2.4.5-$V" -f AddGCSDockerfile .
 docker build --build-arg base=$SPARK_TARGET/spark-r:v2.4.5 -t "${SPARK_TARGET}/spark-r-with-gcs:v2.4.5-$V" -f AddGCSDockerfile .
-docker build --build-arg base=$SPARK_TARGET/spark-py:v2.4.5 -t "${SPARK_TARGET}/spark-py-with-gcs:v2.4.5-$V" -f AddGCSDockerfile .
+SPARK_PY_WORKER=${SPARK_TARGET}/spark-py:v2.4.5
+docker build --build-arg base=${SPARK_PY_WORKER} -t "${SPARK_TARGET}/spark-py-with-gcs:v2.4.5-$V" -f AddGCSDockerfile .
 docker push "${SPARK_TARGET}/spark-with-gcs:v2.4.5-$V"
 docker push "${SPARK_TARGET}/spark-r-with-gcs:v2.4.5-$V"
 docker push "${SPARK_TARGET}/spark-py-with-gcs:v2.4.5-$V"
 rm -rf ${tmp_dir}
+
+echo "Spark notebook pushed to ${TARGET}"
+echo "Spark py worker pushed to ${SPARK_PY_WORKER}"
