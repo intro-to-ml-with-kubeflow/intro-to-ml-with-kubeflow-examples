ebc80d3befa79cc675fcf78a5bfc884249829cab
Holden Karau <holden@pigscanfly.ca>
2020-02-24 15:59:37 -0800

    Spark on KF in Jupyter works now (Next up minio)

--- data-extraction/python-spark-notebook/build.sh
+++ data-extraction/python-spark-notebook/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Build a notebook with Spark
 set -ex
-V=${V:-"10"}
+V=${V:-"12"}
 REPO=${REPO:-"gcr.io/$PROJECT"}
 TARGET=${TARGET:-"$REPO/kubeflow/spark-notebook:v$V"}
 BASE=${BASE:-"gcr.io/kubeflow-images-public/tensorflow-1.15.2-notebook-cpu:1.0.0"}
@@ -15,19 +15,24 @@ pushd ${tmp_dir}
 wget https://www-us.apache.org/dist/spark/spark-2.4.5/spark-2.4.5-bin-hadoop2.7.tgz || wget https://www-eu.apache.org/dist/spark/spark-2.4.5/spark-2.4.5-bin-hadoop2.7.tgz
 tar -xvf spark-2.4.5-bin-hadoop2.7.tgz
 pushd spark-2.4.5-bin-hadoop2.7
-./bin/docker-image-tool.sh -r $SPARK_TARGET -t v2.4.5 build
+./bin/docker-image-tool.sh -r $SPARK_TARGET -t v2.4.5 -b java_image_tag=8 build
 ./bin/docker-image-tool.sh -r $SPARK_TARGET -t v2.4.5 push
 popd
 popd
 # Add GCS to Spark images
 docker build --build-arg base=$SPARK_TARGET/spark:v2.4.5 -t "${SPARK_TARGET}/spark-with-gcs:v2.4.5-$V" -f AddGCSDockerfile .
 docker build --build-arg base=$SPARK_TARGET/spark-r:v2.4.5 -t "${SPARK_TARGET}/spark-r-with-gcs:v2.4.5-$V" -f AddGCSDockerfile .
-SPARK_PY_WORKER=${SPARK_TARGET}/spark-py:v2.4.5
-docker build --build-arg base=${SPARK_PY_WORKER} -t "${SPARK_TARGET}/spark-py-with-gcs:v2.4.5-$V" -f AddGCSDockerfile .
+PYSPARK_WITH_GCS="${SPARK_TARGET}/spark-py-with-gcs:v2.4.5-$V"
+docker build --build-arg base=$SPARK_TARGET/spark-py:v2.4.5 -t ${PYSPARK_WITH_GCS} -f AddGCSDockerfile .
+# Add Python 3.6 to Spark images
+SPARK_PY36_WORKER="${SPARK_TARGET}/spark-py-36:v2.4.5-$V"
+docker build --build-arg base=${PYSPARK_WITH_GCS} -t ${SPARK_PY36_WORKER} -f AddPython3.6Dockerfile .
+
 docker push "${SPARK_TARGET}/spark-with-gcs:v2.4.5-$V"
 docker push "${SPARK_TARGET}/spark-r-with-gcs:v2.4.5-$V"
 docker push "${SPARK_TARGET}/spark-py-with-gcs:v2.4.5-$V"
+docker push "${SPARK_PY36_WORKER}"
 rm -rf ${tmp_dir}
 
 echo "Spark notebook pushed to ${TARGET}"
-echo "Spark py worker pushed to ${SPARK_PY_WORKER}"
+echo "Spark py worker pushed to ${SPARK_PY36_WORKER}"
