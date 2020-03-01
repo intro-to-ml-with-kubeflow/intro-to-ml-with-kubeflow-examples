b22e3a6929a07981c33827b135fd1c6b6f3942bc
Holden Karau <holden@pigscanfly.ca>
2020-02-29 20:04:31 -0800

    Start minio stuff

---
+++ ch03/mac_install.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+brew install minio/stable/minio
---
+++ ch03/minio.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+# Minio runs on port 9000, expose locally to use mc
+kubectl port-forward -n kubeflow svc/minio-service 9000:9000
+# The UI runs on port ? expose locally to use the web UI
+kubectl port-forward -n kubeflow svc/minio-service ?:?
+
+# Kubeflow creates a minio user with password minio123 at install
+mc config host add minio http://localhost:9000 minio minio123
+
+mc ls minio
+# Output [2018-12-13 18:23:41 CST]     0B mlpipeline/
+
+# Make a new bucket for our work
+minio mb kf-book-examples
