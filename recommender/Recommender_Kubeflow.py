50bfdf7aeffcdec6b98fcc492cc81f5743a68299
Holden Karau <holden@pigscanfly.ca>
2020-03-01 17:29:59 -0800

    Commit @blublinsky Upgrade to 1.0 based on https://github.com/intro-to-ml-with-kubeflow/intro-to-ml-with-kubeflow-examples/pull/105

--- recommender/Recommender_Kubeflow.py
+++ recommender/Recommender_Kubeflow.py
@@ -12,42 +12,46 @@
 # 
 # Implementation is leveraging Minio for storing both source data and result models
 # 
-
-# # 1. Install libraries
-
-# In[1]:
-
-
-get_ipython().system('pip3 install --upgrade pip')
-get_ipython().system('pip3 install pandas --upgrade')
-get_ipython().system('pip3 install keras --upgrade')
-get_ipython().system('pip3 install minio --upgrade')
-
+# It also uses Python kubernetes client for re starting model server pod
+# 
 
 # ## imports
 
-# In[2]:
-
-
 import pandas as pd
 import numpy as np
+import time
 from minio import Minio
 from keras.models import Model
 from keras.layers import *
 from keras.losses import *
 import tensorflow as tf
 import os
+from kfmd import metadata
+from datetime import datetime
 from keras import backend as K
+from kubernetes import client as k8s_client, config as k8s_config
 
+# Create a workspace, run and execution
 
-# # 2. Read data
-# 
-# For reading data we are using two diffierent approaches:
-# 1. We use Tensorflow build in support to write resulting model to Minio
-# 2. We use Minio APIs to read source data using Pandas. We could of use Boto APIs here instead.
+execTime = datetime.utcnow().isoformat("T")
+ws = metadata.Workspace(
+    # Connect to metadata-service in namesapce kubeflow in k8s cluster.
+    backend_url_prefix="metadata-service.kubeflow.svc.cluster.local:8080",
+    name="recommender",
+    description="a workspace for saving recommender experiments")
+r = metadata.Run(
+    workspace=ws,
+    name="run-" + execTime ,
+    description="recommender run",
+)
+exec = metadata.Execution(
+    name = "execution" + execTime ,
+    workspace=ws,
+    run=r,
+    description="recommender ML execution",
+)
 
-# In[3]:
-
+# 2. Read data
 
 minio_endpoint = os.environ.get('MINIO_URL', 'minio-service.kubeflow.svc.cluster.local:9000')
 minio_key = os.environ.get('MINIO_KEY', 'minio')
@@ -63,10 +67,6 @@ os.environ['S3_ENDPOINT'] = minio_endpoint
 os.environ['S3_USE_HTTPS'] = '0'
 os.environ['S3_VERIFY_SSL'] = '0'
 
-
-# In[4]:
-
-
 minioClient = Minio(minio_endpoint,
                     access_key=minio_key,
                     secret_key=minio_secret,
@@ -77,38 +77,25 @@ customers = pd.read_csv('/tmp/users.csv')
 minioClient.fget_object('data', 'recommender/transactions.csv', '/tmp/transactions.csv')
 transactions = pd.read_csv('/tmp/transactions.csv')
 
-
-# In[5]:
-
+#Log experiment data set
+data_set = exec.log_input(
+        metadata.DataSet(
+            description="recommender current transactions and customers",
+            name="Current transactions and customers",
+            version=execTime,
+            uri="minio:/tmp/transactions.csv; minio:/tmp/users.csv"))
 
 print(customers.shape)
 customers.head()
-
-
-# In[6]:
-
-
 print(transactions.shape)
 transactions.head()
 
-
-# # 3 Data preparation
-# 
-# Our goal here is to break down each list of items in the products column into rows 
-# and count the number of products bought by a user
-
-# In[7]:
-
-
-# 1: split product items
+# 3 Data preparation
+# split product items
 transactions['products'] = transactions['products'].apply(lambda x: [int(i) for i in x.split('|')])
 transactions.head(2).set_index('customerId')['products'].apply(pd.Series).reset_index()
 
-
-# In[8]:
-
-
-# 2: organize a given table into a dataframe with customerId, single productId, and purchase count
+# organize a given table into a dataframe with customerId, single productId, and purchase count
 pd.melt(transactions.head(2).set_index('customerId')['products'].apply(pd.Series).reset_index(), 
              id_vars=['customerId'],
              value_name='products') \
@@ -119,12 +106,7 @@ pd.melt(transactions.head(2).set_index('customerId')['products'].apply(pd.Series
     .reset_index() \
     .rename(columns={'products': 'productId'})
 
-
-# ## 3.1 Create data with user, item, and target field
-
-# In[9]:
-
-
+# Create data with user, item, and target field
 data = pd.melt(transactions.set_index('customerId')['products'].apply(pd.Series).reset_index(), 
              id_vars=['customerId'],
              value_name='products') \
@@ -139,61 +121,21 @@ data['productId'] = data['productId'].astype(np.int64)
 print(data.shape)
 data.head()
 
-
-# ## 3.2 Create dummy
-# 
-# Dummy for marking whether a customer bought that item or not.
-# If one buys an item, then purchase_dummy are marked as 1
-# Why create a dummy instead of normalizing it, you ask?
-# Normalizing the purchase count, say by each user, would not work because customers may have different buying frequency don't have the same taste
-# However, we can normalize items by purchase frequency across all users, which is done below.
-# 
-# 
-
-# In[10]:
-
-
-def create_data_dummy(data):
-    data_dummy = data.copy()
-    data_dummy['purchase_dummy'] = 1
-    return data_dummy
-data_dummy = create_data_dummy(data)
-
-
-# ## 3.3 Normalize item values across users
-
-# In[11]:
-
-
+# Normalize item values across users
 df_matrix = pd.pivot_table(data, values='purchase_count', index='customerId', columns='productId')
 df_matrix.head()
-
-
-# In[12]:
-
-
 df_matrix_norm = (df_matrix-df_matrix.min())/(df_matrix.max()-df_matrix.min())
 print(df_matrix_norm.shape)
 df_matrix_norm.head()
 
-
-# In[13]:
-
-
 # create a table for input to the modeling
-
 d = df_matrix_norm.reset_index()
 d.index.names = ['scaled_purchase_freq']
 data_norm = pd.melt(d, id_vars=['customerId'], value_name='scaled_purchase_freq').dropna()
 print(data_norm.shape)
 data_norm.head()
 
-
-# # 4 Preparing data for learning
-
-# In[14]:
-
-
+# 4 Preparing data for learning
 customer_idxs = np.array(data_norm.customerId, dtype = np.int)
 product_idxs = np.array(data_norm.productId, dtype = np.int)
 
@@ -211,23 +153,12 @@ print(customer_idxs)
 print(product_idxs)
 print(ratings)
 
-
-# ## 4.1 Tensorflow Session
-
-# In[15]:
-
-
-# create TF session and set it in Keras
+# 4.1 Tensorflow Session. Create TF session and set it in Keras
 sess = tf.Session()
 K.set_session(sess)
 K.set_learning_phase(1)
 
-
-# ## 4.2 Model Class
-
-# In[16]:
-
-
+# 4.2 Model Class
 class DeepCollaborativeFiltering(Model):
     def __init__(self, n_customers, n_products, n_factors, p_dropout = 0.2):
         x1 = Input(shape = (1,), name="user")
@@ -263,41 +194,49 @@ class DeepCollaborativeFiltering(Model):
             np.array([productMapping[product_idx] for product_idx in product_idxs])
         ])
 
-
-# ## 4.3 Hyperparameters
-
-# In[17]:
-
-
+# 4.3 Hyperparameters
 bs = 64
 val_per = 0.25
 epochs = 3
 
-
-# ## 4.4 Model Definition
-
-# In[18]:
-
+# 4.4 Model Definition
 
 model = DeepCollaborativeFiltering(n_customers, n_products, n_factors)
 model.summary()
 
-
-# # 5 Training
-
-# In[19]:
-
-
+# 5 Training
 model.compile(optimizer = 'adam', loss = mean_squared_logarithmic_error)
 model.fit(x = [customer_idxs, product_idxs], y = ratings, batch_size = bs, epochs = epochs, validation_split = val_per)
 print('Done training!')
 
+# 5.1 Log model and metrics
+logmodel = exec.log_output(
+    metadata.Model(
+            name="DeepCollaborativeFiltering",
+            description="Model for product recommender",
+            uri="",
+            model_type="neural network",
+            version=execTime,
+            training_framework={
+                "name": "tensorflow",
+                "version": "v1.14"
+            },
+            hyperparameters={
+                "batch_size" : 64,
+                "validation_split" : 0.25,
+                "layers": [n_customers, n_products, n_factors],
+                "epochs" : 3
+            }))
+metrics = exec.log_output(
+    metadata.Metrics(
+            name="Model for product recommender evaluation",
+            description="Validating of the recommender model",
+            uri="",
+            version=execTime,
+            data_set_id=data_set.id,
+            model_id=logmodel.id))
 
-# # 6 Get current output directory for model
-
-# In[20]:
-
-
+# 6 Get current output directory for model
 directorystream = minioClient.get_object('data', 'recommender/directory.txt')
 directory = ""
 for d in directorystream.stream(32*1024):
@@ -306,12 +245,7 @@ arg_version = "1"
 export_path = 's3://models/' + directory + '/' + arg_version + '/'
 print ('Exporting trained model to', export_path)
 
-
-# ## 6.1 Export models
-
-# In[21]:
-
-
+# 6.1 Export models
 # inputs/outputs
 tensor_info_users = tf.saved_model.utils.build_tensor_info(model.input[0])
 tensor_info_products = tf.saved_model.utils.build_tensor_info(model.input[1])
@@ -320,10 +254,6 @@ tensor_info_pred = tf.saved_model.utils.build_tensor_info(model.output)
 print ("tensor_info_users", tensor_info_users.name)
 print ("tensor_info_products", tensor_info_products.name)
 print ("tensor_info_pred", tensor_info_pred.name)
-
-
-# In[22]:
-
 
 # signature
 prediction_signature = (tf.saved_model.signature_def_utils.build_signature_def(
@@ -341,6 +271,35 @@ builder.add_meta_graph_and_variables(
       legacy_init_op=legacy_init_op)
 builder.save()
 
+# 7 Restarting of the model serving server
+# In order for a new model to take effect it is also necessary to restart a model server.
+# The issue here is that we are not changing the model version version and as a result, 
+# the model will not be updated. To ensure model update, we are here restarting a server -
+# simply killing the running instance, and as a server is installed using deployment, the instance
+# will be recreated. Additionally for pods operations to work correctly from the notebook,
+# it is necessary to create permissions allowing for access to pods in another namespace. 
+# Look at the podaccessroles.yaml for details.
+recommender = "recommendermodelserver-"
+if directory == "recommender1":
+    recommender = "recommendermodelserver1-"
+print("pod prefix ", recommender) 
 
-# In[ ]:
+namespace = "kubeflow"
+print("pod namespace ", namespace) 
 
+# Get full pod name for the current model
+k8s_config.load_incluster_config()
+v1 = k8s_client.CoreV1Api()
+pod_list = v1.list_namespaced_pod(namespace)
+pod = [item.metadata.name for item in pod_list.items if recommender in item.metadata.name][0]
+print("Current pod name ", pod)
+
+# Delete pod, so that it gets recreated
+v1.delete_namespaced_pod(pod, namespace, grace_period_seconds=0)
+print("Done deleting")
+
+# Verify that the new instance was created
+time.sleep(20)
+pod_list = v1.list_namespaced_pod(namespace)
+pod = [item.metadata.name for item in pod_list.items if recommender in item.metadata.name][0]
+print("New pod name ", pod)
