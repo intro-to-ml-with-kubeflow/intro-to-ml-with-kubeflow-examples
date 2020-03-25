f2ec126bed36c842167db27108249e3ae89a31f7
Holden Karau <holden@pigscanfly.ca>
2020-03-24 17:43:51 -0700

    Cleanup the install kubeflow example to not extend out too long

--- dev-setup/install-kf.sh
+++ dev-setup/install-kf.sh
@@ -8,13 +8,18 @@ mkdir -p ~/bin
 export KUBEFLOW_TAG=1.0
 # ^ You can also point this to a different version if you want to try
 KUBEFLOW_BASE="https://api.github.com/repos/kubeflow/kfctl/releases"
-# TODO(holden): Verify if the hash in the file name is now part of the name or if that's just in the RC
-KFCTL_URL=$(curl -s ${KUBEFLOW_BASE} |grep http | grep ${KUBEFLOW_TAG} |grep -i ${PLATFORM}| cut -d : -f 2,3 | tr -d '\" ' )
+# Or just go to https://github.com/kubeflow/kfctl/releases
+KFCTL_URL=$(curl -s ${KUBEFLOW_BASE} |\
+	      grep http |\
+	      grep ${KUBEFLOW_TAG} |\
+	      grep -i ${PLATFORM}|\
+	      cut -d : -f 2,3 |\
+	      tr -d '\" ' )
 wget "${KFCTL_URL}"
 KFCTL_FILE=$(echo ${KFCTL_URL##*/})
 tar -xvf "${KFCTL_FILE}"
 mv ./kfctl ~/bin/
 rm "${KFCTL_FILE}"
-# Optionally add the scripts directory to your path to simplify your work
+# Recommended add the scripts directory to your path
 export PATH=$PATH:~/bin
 #end::install[]
