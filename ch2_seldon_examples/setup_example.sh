c48631bef7040b04ba8ec3de539837556c7a6529
Holden Karau <holden@pigscanfly.ca>
2020-03-24 18:05:34 -0700

    Update the build project to just use the istio one

--- ch2_seldon_examples/setup_example.sh
+++ ch2_seldon_examples/setup_example.sh
@@ -8,7 +8,7 @@ unset ch2_example_path
 ch2_example_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 echo "Using path ${ch2_example_path} for our example path"
 example_path=$(dirname ${ch2_example_path})
-#tag::generate_kf_app[]
+#tag::generate_kf_app_p1[]
 # Pick the correct config file for your platform from
 # https://github.com/kubeflow/manifests/tree/[version]/kfdef
 # You can download & edit the configuration at this point if you need to.
@@ -23,6 +23,7 @@ pushd ${KF_PROJECT_NAME}
 manifest_root=https://raw.githubusercontent.com/kubeflow/manifests/
 # On most enviroments this will create a "vanilla" kubeflow install using istio.
 KFDEF=${manifest_root}${MANIFEST_BRANCH}/kfdef/kfctl_k8s_istio.${MANIFEST_VERSION}.yaml
+#end::generate_kf_app_p1[]
 # On GCP this will create a cluster with basic authentication
 if [ "$PLATFORM" == "gcp" ]; then
   KFDEF=${manifest_root}${MANIFEST_BRANCH}/kfdef/kfctl_gcp_iap.${MANIFEST_VERSION}.yaml
@@ -38,11 +39,11 @@ if [ "$PLATFORM" == "gcp" ]; then
   gcloud config set compute/zone ${ZONE}
 fi
 pwd
-# kfctl build -f $KFDEF -V
+#tag::generate_kf_app_p2[]
 kfctl apply -f $KFDEF -V
 
 popd
-#end::generate_kf_app[]
+#end::generate_kf_app_p2[]
 
 
 # TODO(trevor): what version/tag?
