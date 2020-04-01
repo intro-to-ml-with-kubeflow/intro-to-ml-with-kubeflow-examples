04c5c4e24b33a1a7a9a4c6d083bc800b5aff6ae7
Holden Karau <holden@pigscanfly.ca>
2020-04-01 16:39:46 -0700

    Fix up exports in run_example in ch2

--- ch2_seldon_examples/run_example.sh
+++ ch2_seldon_examples/run_example.sh
@@ -7,9 +7,12 @@ dsl-compile --py train_pipeline.py --output job.yaml
 minikube service --url -n istio-system istio-ingressgateway
 # If your on GCP https://<kf_app_name>.endpoints.<gcp_project_name>.cloud.goog/
 # If you're on vanilla K8s
-export INGRESS_HOST=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
-export INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="http2")].port}')
-export SECURE_INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="https")].port}')
+INGRESS_HOST=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+export INGRESS_HOST
+INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="http2")].port}')
+export INGRESS_PORT
+SECURE_INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="https")].port}')
+export SECURE_INGRESS_PORT
 
 kubectl get svc istio-ingressgateway -n istio-system
 #end::connectToWebUI[]
