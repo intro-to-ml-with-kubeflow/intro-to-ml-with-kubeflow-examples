a3b6652867659460002406ba3d6bcdf323c4c3ef
Holden Karau <holden@pigscanfly.ca>
2020-06-06 18:01:49 -0700

    Fix shell check errors

--- ci.sh
+++ ci.sh
@@ -6,7 +6,7 @@ set -ex
 find ./ -iregex '^.+\.sh$' -type f -print0 | xargs -0 shellcheck -e SC1091 -e SC2164 -e SC1090
 # Check for cases where I use tags rather than tag
 bad_tags=$(grep -r "tags::" ./ | grep -v "ci.sh:")
-if [[ ! -z "$bad_tags" ]]; then
+if [[ -n "$bad_tags" ]]; then
   echo "Found bad tags $bad_tags replace tags with tag"
   exit 1
 fi
