276e6a562322080d33e326609885e3566f2761d0
Holden Karau <holden@pigscanfly.ca>
2020-06-06 18:43:07 -0700

    Check for long lines in shell files

--- ci.sh
+++ ci.sh
@@ -5,9 +5,16 @@ set -ex
 # Check all the shell scripts
 find ./ -iregex '^.+\.sh$' -type f -print0 | xargs -0 shellcheck -e SC1091 -e SC2164 -e SC1090
 # Check for cases where I use tags rather than tag
-bad_tags=$(grep -r "tags::" ./ | grep -v "ci.sh:")
+bad_tags=$(grep -r "tags::" ./ | grep -v "ci.sh:" || true)
+# Look for long lines
+long_lines=$(grep --include '*.sh' -Hnr '.\{80\}' ./ | grep -v "venv" || true)
 if [[ -n "$bad_tags" ]]; then
   echo "Found bad tags $bad_tags replace tags with tag"
+fi
+if [[ -n "$long_lines" ]]; then
+  print "Found long lines:\n$long_lines"
+fi
+if [[ -n "$bad_tags" ]] || [[ -n "$long_lines" ]]; then
   exit 1
 fi
 ./runthrough.sh
