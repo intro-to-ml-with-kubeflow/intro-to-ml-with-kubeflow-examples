cb9a0f54b71e048ae9340853b6fd91548b1777ad
Holden Karau <holden@pigscanfly.ca>
2020-06-06 18:44:04 -0700

    Let it be up to 90

--- ci.sh
+++ ci.sh
@@ -7,7 +7,7 @@ find ./ -iregex '^.+\.sh$' -type f -print0 | xargs -0 shellcheck -e SC1091 -e SC
 # Check for cases where I use tags rather than tag
 bad_tags=$(grep -r "tags::" ./ | grep -v "ci.sh:" || true)
 # Look for long lines
-long_lines=$(grep --include '*.sh' -Hnr '.\{80\}' ./ | grep -v "venv" || true)
+long_lines=$(grep --include '*.sh' -Hnr '.\{90\}' ./ | grep -v "venv" || true)
 if [[ -n "$bad_tags" ]]; then
   echo "Found bad tags $bad_tags replace tags with tag"
 fi
