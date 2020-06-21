cc5cf30212a88f1c5c97d4c65e6d35824f468669
Holden Karau <holden@pigscanfly.ca>
2020-06-21 09:54:27 -0700

    Newer version of argo

--- dev-setup/install-argo.sh
+++ dev-setup/install-argo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Download the binary
-curl -sLO https://github.com/argoproj/argo/releases/download/v2.7.0/argo-linux-amd64
+curl -sLO https://github.com/argoproj/argo/releases/download/v2.8.1/argo-linux-amd64
 
 # Make binary executable
 chmod +x argo-linux-amd64
